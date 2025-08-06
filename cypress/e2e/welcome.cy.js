0351b826217c3969ce65face0f261e54e61d6bd7
unknown <otujacob4@gmail.com>
2025-08-06 10:16:04 +0100

    changed url

--- cypress/e2e/welcome.cy.js
+++ cypress/e2e/welcome.cy.js
@@ -1,6 +1,6 @@
 describe('Verifying that our page is LIVE', () => {
   it('it visits the live app', () => {
-    cy.visit('https://securityy.vercel.app/')
+    cy.visit('https://jacob-nu.vercel.app')
 
     cy.contains('EDO STATE SECURITY SUMMIT').should('be.visible')
     
