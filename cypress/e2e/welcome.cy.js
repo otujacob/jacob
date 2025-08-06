a3ada205cc3dc6e0227fb33493e663a9f11bd769
unknown <otujacob4@gmail.com>
2025-08-06 10:02:39 +0100

    first commit

--- cypress/e2e/welcome.cy.js
+++ cypress/e2e/welcome.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Verifying that our page is LIVE', () => {
+  it('it visits the live app', () => {
+    cy.visit('https://securityy.vercel.app/')
+
+    cy.contains('EDO STATE SECURITY SUMMIT').should('be.visible')
+    
+  })
+
 })
